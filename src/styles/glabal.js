bad14f8081e373e0c621a5ded5de6f5080930c91
SeoWS <seows2@naver.com>
2021-01-08 00:23:51 +0900

    스타일 설정과 변수

---
+++ src/styles/glabal.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from "styled-normalize"
+
+const GlobalStyle = createGlobalStyle`
+    ${normalize}
+
+    @import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap');
+
+    body {
+        font-family: "Noto Sans Kr", sans-serif;
+        padding: 0;
+    }
+    a {
+        text-decoration: none;
+    }
+`
+
+export default GlobalStyle;
